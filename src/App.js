f12adb94b2f3a4a560fe3cc770f1f0ada237b45b
MrNewaz <saifur.saif@northsouth.edu>
2021-02-15 17:28:58 +0600

    part 8

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Home from "./pages/Home";
 function App() {
   return (
     <div className="App">
-      <h1>Hello Ignite</h1>
       <Home />
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,52 @@
 import { useEffect } from "react";
-
 //Redux;
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { loadGames } from "../actions/gamesAction";
+//components
+import Game from "../components/Game";
+//Style & Animation
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const Home = () => {
   //Fetch Games
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(loadGames());
-  });
+  }, [dispatch]);
+  const { popular, newGames, upcoming } = useSelector((state) => state.games);
+  console.log(popular);
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <GameList>
+      <h2>Upcoming Games</h2>
+      <Games>
+        {upcoming.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            id={game.id}
+            image={game.background_image}
+            key={game.id}
+          />
+        ))}
+      </Games>
+    </GameList>
   );
 };
+
+const GameList = styled(motion.div)`
+  padding: 0 5rem;
+  h2 {
+    padding: 3rem 0;
+  }
+`;
+
+const Games = styled(motion.div)`
+  min-height: 80vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+  grid-column-gap: 3rem;
+  grid-row-gap: 3rem;
+`;
 
 export default Home;
