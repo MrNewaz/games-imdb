58c0c1dcd2dfd31211676eea721adb607fb2c51f
MrNewaz <saifur.saif@northsouth.edu>
2021-02-15 18:40:20 +0600

    part 9

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 //Components & Pages
 import Home from "./pages/Home";
+import GlobalStyle from "./components/GlobalStyles";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <Home />
     </div>
   );
---
+++ src/components/GlobalStyles.js
@@ -0,0 +1,55 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+    
+    html {
+        scroll-behavior: smooth;
+
+        &::-webkit-scrollbar{
+            width:0.5rem;
+        }
+        &::-webkit-scrollbar-thumb{
+            border-radius:1rem;
+            background-color: darkgray;
+        }
+    }
+    
+    body {
+        font-family: 'Montserrat', sans-serif;
+        width: 100%;
+    }
+    h2{
+        font-size: 3rem;
+        font-family: 'Abril Fatface', cursive;
+        font-weight:lighter;
+        color: #333;
+    }
+    h3{
+        font-size: 1.1rem;
+        color: #333;
+        padding:1.5rem;
+    }
+    p{
+        font-size: 1rem;
+        line-height: 200%;
+        color: #696969;
+    }
+    
+    a {
+      text-decoration: none;
+      color: #333;
+    }
+    
+    ul {
+      list-style: none;
+    }
+
+`;
+
+export default GlobalStyle;
