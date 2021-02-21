ff82e30ab48d39e2755bca74eddafe270d440926
MrNewaz <saifur.saif@northsouth.edu>
2021-02-22 01:49:49 +0600

    part 21

--- src/api.js
+++ src/api.js
@@ -42,3 +42,6 @@ export const gameDetailsURL = (game_id) => `${base_url}games/${game_id}`;
 //Games ScreenShot
 export const gameScreenshotURL = (game_id) =>
   `${base_url}games/${game_id}/screenshots`;
+//Search Game
+export const searchGameURL = (game_name) =>
+  `${base_url}games?search=${game_name}&page_size=9`;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,38 @@
+import { useState } from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
 import logo from "../img/logo.svg";
+//Redux & Routes
+import { fetchSearch } from "../actions/gamesAction";
+import { useDispatch } from "react-redux";
 
 const Nav = () => {
+  const dispatch = useDispatch();
+  const [textInput, setTextInput] = useState("");
+  const inputHandler = (e) => {
+    setTextInput(e.target.value);
+  };
+  const submitSearch = (e) => {
+    e.preventDefault();
+    dispatch(fetchSearch(textInput));
+    setTextInput("");
+  };
+
+  const clearSearched = () => {
+    dispatch({ type: "CLEAR_SEARCHED" });
+  };
   return (
     <StyledNav>
-      <Logo>
+      <Logo onClick={clearSearched}>
         <img src={logo} alt="logo" />
         <h1>Ignite</h1>
       </Logo>
-      <div className="search">
-        <input type="text" />
-        <button>Search</button>
-      </div>
+      <form className="search">
+        <input value={textInput} onChange={inputHandler} type="text" />
+        <button onClick={submitSearch} type="submit">
+          Search
+        </button>
+      </form>
     </StyledNav>
   );
 };
@@ -36,6 +56,7 @@ const StyledNav = styled(motion.nav)`
     cursor: pointer;
     background: #ff7676;
     color: #ffffff;
+    box-shadow: 0px 0px 30px rgba(0, 0, 0, 0.2);
   }
 `;
 const Logo = styled(motion.div)`
