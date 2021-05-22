6a734e7bfdc6797d3a6571cf1f35b3bcd6dda374
MrNewaz <saifur.saif@northsouth.edu>
2021-05-23 02:38:22 +0600

    push

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,36 +1,36 @@
-import { useState } from "react";
-import styled from "styled-components";
-import { motion } from "framer-motion";
-import logo from "../img/logo.svg";
+import { useState } from 'react';
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+import logo from '../img/logo.svg';
 //Redux & Routes
-import { fetchSearch } from "../actions/gamesAction";
-import { useDispatch } from "react-redux";
-import { fadeIn } from "../animations";
+import { fetchSearch } from '../actions/gamesAction';
+import { useDispatch } from 'react-redux';
+import { fadeIn } from '../animations';
 
 const Nav = () => {
   const dispatch = useDispatch();
-  const [textInput, setTextInput] = useState("");
+  const [textInput, setTextInput] = useState('');
   const inputHandler = (e) => {
     setTextInput(e.target.value);
   };
   const submitSearch = (e) => {
     e.preventDefault();
     dispatch(fetchSearch(textInput));
-    setTextInput("");
+    setTextInput('');
   };
 
   const clearSearched = () => {
-    dispatch({ type: "CLEAR_SEARCHED" });
+    dispatch({ type: 'CLEAR_SEARCHED' });
   };
   return (
-    <StyledNav variants={fadeIn} initial="hidden" animate="show">
+    <StyledNav variants={fadeIn} initial='hidden' animate='show'>
       <Logo onClick={clearSearched}>
-        <img src={logo} alt="logo" />
-        <h1>Ignite</h1>
+        <img src={logo} alt='logo' />
+        <h1>Newaz</h1>
       </Logo>
-      <form className="search">
-        <input value={textInput} onChange={inputHandler} type="text" />
-        <button onClick={submitSearch} type="submit">
+      <form className='search'>
+        <input value={textInput} onChange={inputHandler} type='text' />
+        <button onClick={submitSearch} type='submit'>
           Search
         </button>
       </form>
